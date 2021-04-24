f46726d85a6f14e0f34c7c243094b3b13c6e4a5e
eddanieles <eddanieles@gmail.com>
2021-04-23 23:21:59 -0400

    wireframed candidates page

---
+++ src/views/Candidates.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="dashboard-page">
+    <h1 class="page-title">Candidates</h1>
+    <button @click="logout()">Logout</button>
+    
+    <b-row>
+        <b-col xs="12">
+          <Widget
+            title="<h5>Support <span class='fw-semi-bold'>Requests</span></h5>"
+            bodyClass="widget-table-overflow"
+            customHeader
+          >
+            <div class="table-responsive">
+              <table class="table table-striped table-lg mb-0 requests-table">
+                <thead>
+                  <tr class="text-muted">
+                    <th>NAME</th>
+                    <th>EMAIL</th>
+                    <th>PRODUCT</th>
+                    <th>PRICE</th>
+                    <th>DATE</th>
+                    <th>CITY</th>
+                    <th>STATUS</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr
+                    v-for="row in mock.table"
+                    :key="row.id"
+                  >
+                    <td>{{row.name}}</td>
+                    <td>{{row.email}}</td>
+                    <td>{{row.product}}</td>
+                    <td>{{row.price}}</td>
+                    <td>{{row.date}}</td>
+                    <td>{{row.city}}</td>
+                    <td>
+                      <b-badge
+                        :variant="row.status === 'Pending'
+                          ? 'success'
+                          : row.status === 'Declined' ? 'danger' : 'info'"
+                        pill
+                      >
+                        {{row.status}}
+                      </b-badge>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </Widget>
+        </b-col>
+      </b-row>
+  </div>
+</template>
+
+<script>
+import Widget from '@/components/Widget/Widget';
+import mock from './mock';
+
+export default {
+    name: 'Candidates',
+    components: {
+        Widget
+    },
+    data() {
+        return {
+            mock,
+            errorMessage: null
+        };
+    },
+    methods: {
+        logout() {
+            this.$store.dispatch('logout')
+        }
+    }
+}
+</script>
+
+<style src="./Dashboard.scss" lang="scss" >
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -113,59 +113,12 @@
         />
       </b-col>
     </b-row>
-    <b-row>
-        <b-col xs="12">
-          <Widget
-            title="<h5>Support <span class='fw-semi-bold'>Requests</span></h5>"
-            bodyClass="widget-table-overflow"
-            customHeader
-          >
-            <div class="table-responsive">
-              <table class="table table-striped table-lg mb-0 requests-table">
-                <thead>
-                  <tr class="text-muted">
-                    <th>NAME</th>
-                    <th>EMAIL</th>
-                    <th>PRODUCT</th>
-                    <th>PRICE</th>
-                    <th>DATE</th>
-                    <th>CITY</th>
-                    <th>STATUS</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr
-                    v-for="row in mock.table"
-                    :key="row.id"
-                  >
-                    <td>{{row.name}}</td>
-                    <td>{{row.email}}</td>
-                    <td>{{row.product}}</td>
-                    <td>{{row.price}}</td>
-                    <td>{{row.date}}</td>
-                    <td>{{row.city}}</td>
-                    <td>
-                      <b-badge
-                        :variant="row.status === 'Pending'
-                          ? 'success'
-                          : row.status === 'Declined' ? 'danger' : 'info'"
-                        pill
-                      >
-                        {{row.status}}
-                      </b-badge>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </Widget>
-        </b-col>
-      </b-row>
+    
   </div>
 </template>
 
 <script>
-import { auth } from '../../firebase'
+import { auth } from '../firebase'
 import Widget from '@/components/Widget/Widget';
 import BigStat from './components/BigStat/BigStat';
 import mock from './mock';
